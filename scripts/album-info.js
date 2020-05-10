7addd43d7682d842e965d3326f532bdc5a85adf5
Andrew <ajuhpark@gmail.com>
2020-05-10 16:02:34 -0700

    mapped the artist and release info from the album object.

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -7,4 +7,10 @@ the src attribute of the img#album-cover-art element
 */
 $('#img#album-cover-art').attr('src', album.albumArtUrl);
 
+//<h2 class="artist"></h2>
+$(‘.artist’).text(album.artist);
+
+// for <div id="release-info"></div>
+$(‘#release-info’).text(album.releaseInfo);
+
 }
