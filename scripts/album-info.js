ee0559dcef41e98c5c77c5455541a7d300eb06f8
Andrew <ajuhpark@gmail.com>
2020-05-16 22:09:46 -0700

    fixing album art

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -7,7 +7,7 @@ $('#album-title').text(album.title);
 Use jQuery's .attr() method to add album.albumArtUrl to
 the src attribute of the img#album-cover-art element
 */
-$('#img#album-cover-art').attr('src', album.albumArtUrl);
+$('img#album-cover-art').attr('src', album.albumArtUrl);
 
 //<h2 class="artist"></h2>
 $('.artist').text(album.artist);
